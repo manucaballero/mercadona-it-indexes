1b3f1d1b5a02860174017b4ac902532eef6a4b2b
Juan Zalacaín García <jzalacai@mercadona.es>
2023-10-04 12:17:49 +0200

    update index creations for hash

--- indexes/info/3a-Hash.sql
+++ indexes/info/3a-Hash.sql
@@ -1,41 +1,33 @@
 
--- Verificar si el índice existe
-DO $$ 
-BEGIN
-    IF EXISTS (SELECT 1 FROM pg_indexes WHERE indexname = 'idx_id' AND tablename = 'ventas') THEN
-        -- Si el índice existe, eliminarlo
-        EXECUTE 'DROP INDEX idx_id';
-    END IF;
-END $$;
-
-
---CREAMOS INDICE
-CREATE INDEX idx_id ON ventas USING btree (id);
-
+--CREAMOS INDICE BTREE
+CREATE INDEX idx_id_btree ON ventas USING btree (id);
 
 --REVISAMOS TIEMPO CON INDICE
 EXPLAIN ANALYZE
 SELECT * FROM ventas WHERE id = 57;
 
-
---BORAMOS EL INDICE BTREE
-DO $$ 
-BEGIN
-    IF EXISTS (SELECT 1 FROM pg_indexes WHERE indexname = 'idx_id' AND tablename = 'ventas') THEN
-        -- Si el índice existe, eliminarlo
-        EXECUTE 'DROP INDEX idx_id';
-    END IF;
-END $$;
-
-
-
 --CREAMOS INDICE HASH 
-CREATE INDEX idx_id ON ventas USING hash (id);
-
+CREATE INDEX idx_id_hash ON ventas USING hash (id);
 
 --REVISAMOS TIEMPO CON INDICE HASH
 EXPLAIN ANALYZE
 SELECT * FROM ventas WHERE id = 58;
 
+--COMPROBAMOS QUE EL COSTE ES MENOR CON EL INDICE HASH Y ADEMAS AUTOMATICAMENTE USA ESTE INDICE
 
---COMPROBAMOS QUE EL COSTE ES MENOR CON EL INDICE HASH
+--BORRAMOS LOS INDICES UTIIZADOS
+DO $$ 
+BEGIN
+    IF EXISTS (SELECT 1 FROM pg_indexes WHERE indexname = 'idx_id_btree' AND tablename = 'ventas') THEN
+        -- Si el índice existe, eliminarlo
+        EXECUTE 'DROP INDEX idx_btree';
+    END IF;
+END $$;
+
+DO $$ 
+BEGIN
+    IF EXISTS (SELECT 1 FROM pg_indexes WHERE indexname = 'idx_id_hash' AND tablename = 'ventas') THEN
+        -- Si el índice existe, eliminarlo
+        EXECUTE 'DROP INDEX idx_hash';
+    END IF;
+END $$;
