e6cdcfdc8d1dfb142dbfe7e9fc82796276a16bf9
Juan Zalacaín García <jzalacai@mercadona.es>
2023-10-05 13:01:54 +0200

    gin improve

--- indexes/info/5a-Gin.sql
+++ indexes/info/5a-Gin.sql
@@ -4,7 +4,7 @@ CREATE INDEX idx_id_btree ON ventas USING btree (descripcion);
 
 --REVISAMOS TIEMPO CON INDICE
 EXPLAIN ANALYZE
-SELECT * FROM ventas WHERE descripcion ILIKE '%oferta%';
+SELECT * FROM ventas WHERE descripcion ILIKE 'oferta%';
 
 --CREAMOS INDICE gin 
 
@@ -14,9 +14,10 @@ CREATE INDEX idx_id_gin ON ventas USING gin (descripcion gin_trgm_ops);
 
 --REVISAMOS TIEMPO CON INDICE GIN
 EXPLAIN ANALYZE
-SELECT * FROM ventas WHERE descripcion ILIKE '%especial%';
+SELECT * FROM ventas WHERE descripcion ILIKE 'especial%';
 
---COMPROBAMOS QUE EL COSTE ES MENOR CON EL INDICE GIN Y ADEMAS AUTOMATICAMENTE USA ESTE INDICE
+--COMPROBAMOS QUE EL COSTE ES MENOR CON EL INDICE GIN Y ADEMAS AUTOMATICAMENTE USA ESTE INDICE (PUEDE HABER CASOS DONDE NO LO USE, DEPENDE DE LA ESTIMACION QUE HAGA
+-- DE CUANTOS ELEMENTOS COINCIDEN)
 
 --BORRAMOS LOS INDICES UTIIZADOS
 DO $$ 
@@ -29,8 +30,8 @@ END $$;
 
 DO $$ 
 BEGIN
-    IF EXISTS (SELECT 1 FROM pg_indexes WHERE indexname = 'idx_id_hash' AND tablename = 'ventas') THEN
+    IF EXISTS (SELECT 1 FROM pg_indexes WHERE indexname = 'idx_id_gin' AND tablename = 'ventas') THEN
         -- Si el índice existe, eliminarlo
-        EXECUTE 'DROP INDEX idx_GIN';
+        EXECUTE 'DROP INDEX idx_gin';
     END IF;
 END $$;
